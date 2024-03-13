ff1058a64deb82f6812f9724c0f54b32a7ad16a4
witoldnowogorski <witold.nowogorski@inksoftware.house>
2024-03-13 23:41:27 +0100

    sources enlargement + new prompt

--- src/sources/sources_loader.py
+++ src/sources/sources_loader.py
@@ -1,38 +1,61 @@
 import os
 import json
+import urllib.request
 
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader, UnstructuredHTMLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from src.utils import config, sources
 
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=config["sources_loader"]["chunk_size"],
+    chunk_overlap=config["sources_loader"]["chunk_overlap"]
+)
+
 def load_sources():
     documents = []
 
-    # Load AGH statutes pdf documents
-    print('Loading AGH statutes')
-    for statute_link in sources['statutes']:
+    # Load pdf documents
+    print('Loading pdfs')
+    for pdf in sources["pdf"]:
         try:
-            statute_docs = load_pdf(statute_link)
+            url = pdf["url"]
+            statute_docs = load_pdf(url)
             documents.extend(statute_docs)
         except Exception as e:
-            print(f"Error while loading doc: {statute_link}, error message: {e}")
+            print(f"Error while loading doc: {url}, error message: {e}")
             pass
+
+    # Load html websites
+    print('Loading htmls')
+    for i, source_url in enumerate(sources["html"]):
+        if i % 50 == 0:
+            print(f'{i} out of {len(sources["html"])} documents loaded')
+        try:
+            doc = load_html(source_url)
+            documents.extend(doc)
+        except Exception as e:
+            print(f"Error while fetching file {source_url}, error message: {e}")
+            continue
+
+    print(f'Total of {len(documents)} loaded')
 
     return documents
 
 def load_pdf(link):
     loader = PyPDFLoader(link)
     data = loader.load()
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=config["sources_loader"]["chunk_size"],
-        chunk_overlap=config["sources_loader"]["chunk_overlap"]
-    )
     docs = text_splitter.split_documents(data)
 
     return docs
 
+def load_html(link):
+    file = 'temp.html'
+    urllib.request.urlretrieve(link, file)
+    loader = UnstructuredHTMLLoader(file)
+    data = loader.load()
+    docs = text_splitter.split_documents(data)
+    os.remove(file)
 
-def load_html():
-    pass
+    return docs
+
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,10 @@
 import json
 import pymongo
 
-with open("config.json") as config_file:
+with open("src/configs/config.json") as config_file:
     config = json.load(config_file)
 
-with open("sources.json") as sources_file:
+with open("src/sources/sources.json") as sources_file:
     sources = json.load(sources_file)
 
 mongo_uri = config["mongo"]["uri"]
